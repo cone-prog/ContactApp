a9cc7f728c0a168bc7f82ef61f60bd5d7e7580b6
cone-code <polanskijtihon@gmail.com>
2025-12-25 04:58:53 +0500

    донастройка

--- Api/Program.cs
+++ Api/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddSingleton<ContactStorage>();
 builder.Services.AddCors(opt =>
 opt.AddPolicy("CorsPolicy", policy =>
 {
-    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins(args[0]);
 })
 );
 
