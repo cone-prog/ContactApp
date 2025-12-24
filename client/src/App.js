c6a72e4ccbe0b8058c3d1353d846e62eb8ad250d
cone-code <polanskijtihon@gmail.com>
2025-12-25 01:00:10 +0500

    создание таблицы

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
-function App() {
+import TableContact from "./layout/TableContact/TableContact";
+
+const App = () => {
   return (
-    <div className="App">
+    <div className="container mt-5">
+      <div className="card">
+        <div className="card-header">
+          <h1>Список контактов</h1>
+        </div>
+        <div className="card-body">
+          <TableContact></TableContact>
+        </div>
+      </div>
+
     </div>
   );
 }
 
-export default App;
+export default App; 
---
+++ client/src/layout/TableContact/Components/RowTableContact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const RowTableContact = () => {
+    return (
+        <tr>
+            <th>1</th>
+            <th>3423432</th>
+            <th>234234</th>
+        </tr>
+    );
+}
+
+export default RowTableContact;
